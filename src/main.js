fe836fc0886fffbf60d1a6d0ceed97a7a5b4c8c7
Obiwan Pelosi <abioyefeyifoluwa@gmail.com>
2022-03-26 12:44:19 +0100

    add changes

--- src/main.js
+++ src/main.js
@@ -60,21 +60,7 @@ if(token){
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 
-//custom directive to close modals 
-Vue.directive('click-outside', {
-  update: function (el, binding, vnode) {
-    el.event = function (event) {
-      if (!(el == event.target || el.contains(event.target))) {
-        vnode.context[binding.expression](event);
-      }
-    };
-    document.body.addEventListener('click', el.event)
 
-  },
-  unbind: function (el) {
-    document.body.removeEventListener('click', el.event)
-  },
-})
 
 
 Vue.config.productionTip = false
