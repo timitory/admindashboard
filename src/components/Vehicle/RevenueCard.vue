0c33cd5fa1cf311667bcb14daa23651cb8f48c7e
Momohpheel <momohmayowa14@gmail.com>
2022-08-02 12:47:18 +0100

    update revenue and commission

--- src/components/Vehicle/RevenueCard.vue
+++ src/components/Vehicle/RevenueCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-8 md:flex md:gap-6">
         <div class="rounded-lg border border-solid border-gray-500 border-t-0">
-            <div class="py-4 px-6  rounded-lg bg-white">
+            <div class="py-7 px-6 rounded-lg bg-white">
               <div class="flex items-center gap-6">
                   <div class="imgdiv border border-solid border-gray-500">
                     <img src="@/assets/images/dashboard/Image 10.png" alt="" class="w-full h-full">
@@ -11,26 +11,28 @@
                     <p class="mt-2 font-bold text-lg">₦13000</p>
                   </div>
               </div>
-              <hr class="mt-4 border-solid border-gray-300">
-          </div>
-        </div>
-        <div class="rounded-lg border mt-10 lg:mt-0 border-solid border-gray-500 border-t-0">
-            <div class="py-4 px-6  rounded-lg bg-white">
-              <div class="flex items-center gap-6">
-                  <div class="imgdiv border border-solid border-gray-500">
-                    <img src="@/assets/images/dashboard/Image 10.png" alt="" class="w-full h-full">
-                  </div>
-                  <div class="">
-                    <p class="">PaddyCover Revenue</p>
-                    <p class="mt-2 font-bold text-lg">₦13000</p>
-                  </div>
-              </div>
-              <hr class="mt-4 border-solid border-gray-300">
+              <!-- <hr class="mt-4 border-solid border-gray-300"> -->
           </div>
         </div>
 
         <div class="rounded-lg border mt-10 lg:mt-0 border-solid border-gray-500 border-t-0">
-            <div class="py-4 px-6  rounded-lg bg-white">
+            <div class="py-7 px-6  rounded-lg bg-white">
+              <div class="flex items-center gap-6">
+                  <div class="imgdiv border border-solid border-gray-500">
+                    <img src="@/assets/images/dashboard/Image 10.png" alt="" class="w-full h-full">
+                  </div>
+                  
+                  <div class="">
+                    <p class="">Total PC Revenue</p>
+                    <p class="mt-2 font-bold text-lg">₦13000</p>
+                  </div>
+              </div>
+              <!-- <hr class="mt-4 border-solid border-gray-300"> -->
+          </div>
+        </div>
+
+        <div class="rounded-lg border mt-10 lg:mt-0 border-solid border-gray-500 border-t-0">
+            <div class="py-7 px-6  rounded-lg bg-white">
               <div class="flex items-center gap-6">
                   <div class="imgdiv border border-solid border-gray-500">
                     <img src="@/assets/images/dashboard/Image 10.png" alt="" class="w-full h-full">
@@ -40,7 +42,22 @@
                     <p class="mt-2 font-bold text-lg">₦13000</p>
                   </div>
               </div>
-              <hr class="mt-4 border-solid border-gray-300">
+              <!-- <hr class="mt-4 border-solid border-gray-300"> -->
+          </div>
+        </div>
+
+        <div class="rounded-lg border mt-10 lg:mt-0 border-solid border-gray-500 border-t-0">
+            <div class="py-7 px-6  rounded-lg bg-white">
+              <div class="flex items-center gap-6">
+                  <div class="imgdiv border border-solid border-gray-500">
+                    <img src="@/assets/images/dashboard/Image 10.png" alt="" class="w-full h-full">
+                  </div>
+                  <div class="">
+                    <p class="">Underwriter Revenue</p>
+                    <p class="mt-2 font-bold text-lg">₦13000</p>
+                  </div>
+              </div>
+              <!-- <hr class="mt-4 border-solid border-gray-300"> -->
           </div>
         </div>
     </div>
