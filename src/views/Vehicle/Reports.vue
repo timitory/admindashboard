bfba9b488e26b33d8ec769d09ee743140959d3bc
Momohpheel <momohmayowa14@gmail.com>
2023-05-24 21:54:25 +0100

    update

--- src/views/Vehicle/Reports.vue
+++ src/views/Vehicle/Reports.vue
@@ -121,6 +121,7 @@ export default {
                 {id: 5, name: 'Success'},
                 {id: 12, name: 'Pending'},
                 {id: 19, name: 'Incomplete'},
+                {id: 22, name: 'Cancelled'},
             ],
             months: [
                 {id: 1, name: "Jan"},{id: 2, name: "Feb"}, {id: 3, name: "Mar"}, {id: 4, name: "Apr"}, {id: 5, name: "May"}, {id: 6, name: "Jun"}, {id: 7, name: "Jul"}, {id: 8, name: "Aug"}, {id: 9, name: "Sep"}, {id: 10, name: "Oct"}, {id: 11, name: "Nov"}, {id: 12, name: "Dec"},
